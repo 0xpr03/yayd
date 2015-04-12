40eccfe1ac6d19a56526fb0ecf4f5fb0d7307785
Aron Heinecke <Ox0p54r36@t-online.de>
2015-04-12 11:36:20 -0700

    milestone #1 closed with this, basic progress etc working

--- src/lib/downloader.rs
+++ src/lib/downloader.rs
@@ -3,12 +3,14 @@ extern crate regex;
 
 use mysql::conn::pool::MyPool;
 use mysql::conn::pool::MyPooledConn;
+use mysql::value::ToValue;
 use mysql::conn::Stmt;
 
 use std::process::{Command, Stdio};
 use std::error::Error;
 use std::io::prelude::*;
 use std::io::BufReader;
+
 
 #[derive(Debug)]
 pub enum DownloadError {
@@ -39,23 +41,33 @@ pub fn download_video(url: &str, quality: i32, qid: i64, folderFormat: &str, poo
 	let mut stdout = BufReader::new(process.stdout.unwrap());
 
 	let mut conn = pool.get_conn().unwrap();
-	let statement = prepare_progress_updater(&mut conn);
-
+	let mut statement = prepare_progress_updater(&mut conn);
+	let mut conn = pool.get_conn().unwrap();
 	let re = create_regex(&r"\d+\.\d%");
 
 	let i = 0;
+
+	set_query_code(&mut conn, &1, &qid);
 
 	for line in &mut stdout.lines(){
 		match line{
 			Err(why) => panic!("couldn't read cmd stdout: {}", Error::description(&why)),
 			Ok(text) => {
 					println!("Out: {}",text);
-					if re.is_match(&text) {
-						println!("Match: {:?}", text);
+					match re.find(&text) {
+						Some(s) => { println!("Match at {}", s.0);
+									println!("{}", &text[s.0..s.1]); // ONLY with ASCII chars makeable!
+									update_progress(&mut statement, &text[s.0..s.1].to_string(), &qid);
+								},
+						None => println!("Detected no % match."),
 					}
+					//if re.is_match(&text) {println!("Match: {:?}", text);}
 				},
 		}
 	}
+
+	update_progress(&mut statement, &"Finished".to_string(), &qid);
+	set_query_code(&mut conn, &3, &qid);
 
 	Ok(true)
 }
@@ -67,11 +79,17 @@ fn create_regex(expression: & str) -> regex::Regex {
 	}
 }
 
+// MyPooledConn does only live when MyOpts is alive -> lifetime needs to be declared
 fn prepare_progress_updater<'a>(conn: &'a mut MyPooledConn) -> Stmt<'a> {
 	conn.prepare("UPDATE querydetails SET status = ? WHERE qid = ?").unwrap()
 }
 
+fn set_query_code<'a>(conn: &'a mut MyPooledConn, code: &i8, qid: &i64){
+	let mut stmt = conn.prepare("UPDATE querydetails SET code = ? WHERE qid = ?").unwrap();
+	stmt.execute(&[code,qid]);
+}
+
 ///updater called from the stdout progress
-fn update_progress(stmt: &mut Stmt, progress: &String, qid: &String){
+fn update_progress(stmt: &mut Stmt, progress: &String, qid: &i64){
 	stmt.execute(&[progress,qid]);
 }
