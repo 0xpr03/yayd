4f4d9d38f8e24f2fc315b45c8baf9c41e9bab8c8
Aron Heinecke <Ox0p54r36@t-online.de>
2015-07-31 23:17:43 +0200

    *moved independent functions to lib

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -3,3 +3,105 @@ pub mod downloader;
 //pub mod socket;
 pub mod database;
 pub mod converter;
+
+use mysql::conn::pool;
+use mysql::conn::pool::MyPooledConn;
+use lib::downloader::DownloadError;
+use lib::downloader::Downloader;
+
+use std::fs::{rename};
+
+///Return whether the quality is a split container or not: video only
+///as specified in the docs
+fn is_split_container(quality: &i16) -> bool {
+    match *quality {
+        141 | 83 | 82 | 84 | 85 => false,
+        _ => true,
+    }
+}
+
+///Set the state of the current query, code dependent, see QueryCodes
+fn set_query_state(pool: & pool::MyPool,qid: &i64 , state: &str, finished: bool){ // same here
+    let mut conn = pool.get_conn().unwrap();
+    let progress: i32 = if finished {
+        100
+    }else{
+        0
+    };
+    let mut stmt = conn.prepare("UPDATE querydetails SET status = ? , progress = ? WHERE qid = ?").unwrap();
+    let result = stmt.execute(&[&state,&progress,qid]); // why is this var needed ?!
+    match result {
+        Ok(_) => (),
+        Err(why) => println!("Error setting query state: {}",why),
+    }
+}
+
+///Update status code for query entry
+fn set_query_code(conn: & mut MyPooledConn, code: &i8, qid: &i64) -> Result<(), DownloadError> { // same here
+    let mut stmt = conn.prepare("UPDATE querydetails SET code = ? WHERE qid = ?").unwrap();
+    let result = stmt.execute(&[code,qid]);
+    match result {
+        Ok(_) => Ok(()),
+        Err(why) => Err(DownloadError::DBError(why.description().into())),
+    }
+}
+
+///Update progress steps for db entry
+fn update_steps(pool: & pool::MyPool, qid: &i64, step: i32, max_steps: i32){
+    let finished = if max_steps == step {
+        true
+    }else{
+        false
+    };
+    set_query_state(&pool,qid, &format!("{}|{}", step, max_steps), finished);
+}
+
+///add file to db including it's name & fid based on qid
+fn add_file_entry(pool: & pool::MyPool, fid: &i64, name: &str, real_name: &str){
+    println!("name: {}",name);
+    let mut conn = pool.get_conn().unwrap();
+    let mut stmt = conn.prepare("INSERT INTO files (fid,name,rname,valid) VALUES (?,?,?,?)").unwrap();
+    let result = stmt.execute(&[fid,&real_name,&name,&1]); // why is this var needed ?!
+    match result {
+        Ok(_) => (),
+        Err(why) => println!("Error adding file: {}",why),
+    }
+}
+
+///Format file name for 
+fn format_file_name<'a>(name: &str, download: &'a Downloader, qid: &i64) -> String {
+    println!("Fileextension: {:?}", get_file_ext(download));
+    format!("{}-{}.{}",url_encode(name), qid, get_file_ext(download))
+}
+
+///Returns the file extension
+fn get_file_ext<'a>(download: &Downloader) -> &'a str {
+    match download.is_audio() {
+        true => "mp3",
+        false => "mp4",
+    }
+}
+
+///Move file to location
+fn move_file(original: &str, destination: &str) -> Result<(),DownloadError> {
+    match rename(original, destination) { // no try possible..
+        Err(v) => Err(v.into()),
+        Ok(_) => Ok(()),
+    }
+}
+
+///Return an sanitized String (url encode still required)
+fn url_encode(input: &str) -> String {
+    // iterator over input, apply function to each element(function
+    input.chars().map(|char| {
+        match char {
+            '\'' | '"' => '_',
+            '&' => '-',
+            c if c.is_ascii() => c,
+            _ => '_'
+        }
+    }).collect()
+    // match for each char, then do collect: loop through the iterator, collect all elements
+    // into container FromIterator
+}
+
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use lib::downloader::Downloader;
 use lib::downloader::DownloadError;
 use lib::converter::Converter;
 
-use std::fs::{rename,remove_file};
+use std::fs::{remove_file};
 
 static VERSION : &'static str = "0.1"; // String not valid
 static SLEEP_MS: u32 = 5000;
@@ -59,8 +59,8 @@ fn main() {
                 //TODO: set playlist entry to errg
             }
             let qid = result.qid.clone();                 //&QueryCodes::InProgress as i32
-            set_query_code(&mut pool.get_conn().unwrap(), &1, &result.qid).ok().expect("Failed to set query code!");
-            set_query_state(&pool.clone(),&qid, "started", false);
+            lib::set_query_code(&mut pool.get_conn().unwrap(), &1, &result.qid).ok().expect("Failed to set query code!");
+            lib::set_query_state(&pool.clone(),&qid, "started", false);
             let succes = match handle_download(result, None, &converter) {
                 Ok(v) => v,
                 Err(e) => {println!("Error: {:?}", e); false }
@@ -70,13 +70,13 @@ fn main() {
             } else {
                 3//QueryCodes::Failed as i32
             };
-            set_query_code(&mut pool.get_conn().unwrap(), &code,&qid).ok().expect("Failed to set query code!");
+            lib::set_query_code(&mut pool.get_conn().unwrap(), &code,&qid).ok().expect("Failed to set query code!");
             let state = if code == 2 {
                 "finished"
             } else {
                 "failed"
             };
-            set_query_state(&pool.clone(),&qid, state, true);
+            lib::set_query_state(&pool.clone(),&qid, state, true);
         } else {
             if print_pause { println!("Pausing.."); print_pause = false; }
             std::thread::sleep_ms(SLEEP_MS);
@@ -106,7 +106,7 @@ fn handle_download(downl_db: DownloadDB, folder: Option<String>, converter: &Con
     };
 
     let dbcopy = downl_db.clone(); //copy, all implement copy & no &'s in use
-    update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 1, 0);
+    lib::update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 1, 0);
     let download = Downloader::new(downl_db, &CONFIG.general);
     //get filename, check for DMCA
     let mut dmca = false; // "succ." dmca -> file already downloaded
@@ -125,7 +125,7 @@ fn handle_download(downl_db: DownloadDB, folder: Option<String>, converter: &Con
         },
     };
 
-    let name_http_valid = format_file_name(&name, &download, &dbcopy.qid);
+    let name_http_valid = lib::format_file_name(&name, &download, &dbcopy.qid);
 
     let file_path = format_file_path(&dbcopy.qid, folder.clone());
     let save_path = &format_save_path(folder.clone(),&name, &download, &dbcopy.qid);
@@ -136,26 +136,26 @@ fn handle_download(downl_db: DownloadDB, folder: Option<String>, converter: &Con
         //TODO: insert title name for file,
         //copy file to download folder
     
-        let is_splitted_format = is_split_container(&dbcopy.quality);
+        let is_splitted_format = lib::is_split_container(&dbcopy.quality);
         let total_steps = if is_splitted_format {
             4
         } else {
             2
         };
-        update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 2, total_steps);
+        lib::update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 2, total_steps);
 
         //download video, which is video/audio(m4a)
         try!(download.download_file(&file_path, false));
 
         if is_splitted_format { // download audio file & convert together
-            update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 3, total_steps);
+            lib::update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 3, total_steps);
 
             let audio_path = format_audio_path(&dbcopy.qid, folder.clone());
             
             println!("Downloading audio.. {}", audio_path);
             try!(download.download_file(&audio_path, true));
 
-            update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 4, total_steps);
+            lib::update_steps(&dbcopy.pool.clone(),&dbcopy.qid, 4, total_steps);
 
             match converter.merge_files(&dbcopy.qid,&file_path, &audio_path,&save_path) {
                 Err(e) => {println!("merge error: {:?}",e); return Err(e);},
@@ -168,46 +168,20 @@ fn handle_download(downl_db: DownloadDB, folder: Option<String>, converter: &Con
             if download.is_audio(){ // if audio-> convert m4a to mp3, which converts directly to downl. dir
                 //TODO: convert -> saves already ?
             }else{
-                try!(move_file(&file_path, &save_path));
+                try!(lib::move_file(&file_path, &save_path));
             }
         }
         try!(remove_file(&file_path));
     }else{
-        try!(move_file(&file_path, &save_path));
+        try!(lib::move_file(&file_path, &save_path));
     }
 
     if !is_zipped { // add file to list, except it's for zip-compression later (=folder set)
-        add_file_entry(&dbcopy.pool.clone(), &dbcopy.qid,&name_http_valid, &name);
+        lib::add_file_entry(&dbcopy.pool.clone(), &dbcopy.qid,&name_http_valid, &name);
     }
     
     //TODO: download file, convert if audio ?!
     Ok(true)
-}
-
-///Update progress steps for db entry
-fn update_steps(pool: & pool::MyPool, qid: &i64, step: i32, max_steps: i32){
-    let finished = if max_steps == step {
-        true
-    }else{
-        false
-    };
-    set_query_state(&pool,qid, &format!("{}|{}", step, max_steps), finished);
-}
-
-///Returns the file extension
-fn get_file_ext<'a>(download: &Downloader) -> &'a str {
-    match download.is_audio() {
-        true => "mp3",
-        false => "mp4",
-    }
-}
-
-///Move file to location
-fn move_file(original: &str, destination: &str) -> Result<(),DownloadError> {
-    match rename(original, destination) { // no try possible..
-        Err(v) => Err(v.into()),
-        Ok(_) => Ok(()),
-    }
 }
 
 ///Format save location for file, zip dependent
@@ -229,67 +203,9 @@ fn format_audio_path(qid: &i64, folder: Option<String>) -> String {
 ///Format save path, dependent on zip option.
 fn format_save_path<'a>(folder: Option<String>, name: &str, download: &'a Downloader, qid: &i64) -> String {
     match folder {
-        Some(v) => format!("{}/{}/{}", &CONFIG.general.save_dir, v, format_file_name(name,download,qid)),
-        None => format!("{}/{}", &CONFIG.general.download_dir, format_file_name(name,download,qid)),
+        Some(v) => format!("{}/{}/{}", &CONFIG.general.save_dir, v, lib::format_file_name(name,download,qid)),
+        None => format!("{}/{}", &CONFIG.general.download_dir, lib::format_file_name(name,download,qid)),
     }
-}
-
-///Format file name for 
-fn format_file_name<'a>(name: &str, download: &'a Downloader, qid: &i64) -> String {
-    println!("Fileextension: {:?}", get_file_ext(download));
-    format!("{}-{}.{}",url_encode(name), qid, get_file_ext(download))
-}
-
-///Set the state of the current query, code dependent, see QueryCodes
-fn set_query_state(pool: & pool::MyPool,qid: &i64 , state: &str, finished: bool){ // same here
-    let mut conn = pool.get_conn().unwrap();
-    let progress: i32 = if finished {
-        100
-    }else{
-        0
-    };
-    let mut stmt = conn.prepare("UPDATE querydetails SET status = ? , progress = ? WHERE qid = ?").unwrap();
-    let result = stmt.execute(&[&state,&progress,qid]); // why is this var needed ?!
-    match result {
-        Ok(_) => (),
-        Err(why) => println!("Error setting query state: {}",why),
-    }
-}
-
-///add file to db including it's name & fid based on qid
-fn add_file_entry(pool: & pool::MyPool, fid: &i64, name: &str, real_name: &str){
-    println!("name: {}",name);
-    let mut conn = pool.get_conn().unwrap();
-    let mut stmt = conn.prepare("INSERT INTO files (fid,name,rname,valid) VALUES (?,?,?,?)").unwrap();
-    let result = stmt.execute(&[fid,&real_name,&name,&1]); // why is this var needed ?!
-    match result {
-        Ok(_) => (),
-        Err(why) => println!("Error adding file: {}",why),
-    }
-}
-
-///Return whether the quality is a split container or not: video only
-///as specified in the docs
-fn is_split_container(quality: &i16) -> bool {
-    match *quality {
-        141 | 83 | 82 | 84 | 85 => false,
-        _ => true,
-    }
-}
-
-///Return an sanitized String (url encode still required)
-fn url_encode(input: &str) -> String {
-    // iterator over input, apply function to each element(function
-    input.chars().map(|char| {
-        match char {
-            '\'' | '"' => '_',
-            '&' => '-',
-            c if c.is_ascii() => c,
-            _ => '_'
-        }
-    }).collect()
-    // match for each char, then do collect: loop through the iterator, collect all elements
-    // into container FromIterator
 }
 
 ///Request an entry from the DB to handle
@@ -309,24 +225,14 @@ fn request_entry(pool: & pool::MyPool) -> Option<DownloadDB> {
     println!("Result: {:?}", result[0]);
     println!("result str: {}", result[1].into_str());
     let download_db = DownloadDB { url: from_value::<String>(&result[1]),
-                                                quality: from_value::<i16>(&result[3]),
-                                                qid: from_value::<i64>(&result[0]),
-                                                audioquality: CONFIG.codecs.audio,
-                                                folder: CONFIG.general.save_dir.clone(),
-                                                pool: pool.clone(),
-                                                playlist: false, //TEMP
-                                                compress: false };
+                                    quality: from_value::<i16>(&result[3]),
+                                    qid: from_value::<i64>(&result[0]),
+                                    audioquality: CONFIG.codecs.audio,
+                                    folder: CONFIG.general.save_dir.clone(),
+                                    pool: pool.clone(),
+                                    playlist: false, //TEMP
+                                    compress: false };
     Some(download_db)
-}
-
-///Update status code for query entry
-fn set_query_code(conn: & mut MyPooledConn, code: &i8, qid: &i64) -> Result<(), DownloadError> { // same here
-    let mut stmt = conn.prepare("UPDATE querydetails SET code = ? WHERE qid = ?").unwrap();
-    let result = stmt.execute(&[code,qid]);
-    match result {
-        Ok(_) => Ok(()),
-        Err(why) => Err(DownloadError::DBError(why.description().into())),
-    }
 }
 
 ///Set dbms connection settings
