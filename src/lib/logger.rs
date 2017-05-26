11a48e76ed484bf97f1e16ab3299667b34f8a80b
Aron Heinecke <aron.heinecke@t-online.de>
2017-05-26 22:37:16 +0200

    Upgraded & re-enabled log fallback init

--- src/lib/logger.rs
+++ src/lib/logger.rs
@@ -8,17 +8,26 @@ use std::fs::metadata;
 use std::default::Default;
 use std::path::Path;
 
+use log::LogLevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::config::{Appender, Config, Logger, Root};
+
+const APPENDER_FILE: &'static str = "file";
+const APPENDER_STDOUT: &'static str = "stdout";
+
 /// Initializes the logger.
 #[cfg(not(test))]
 pub fn initialize() {
     let mut log_path = get_executable_folder().unwrap_or(std::path::PathBuf::from("/"));
     log_path.push(LOG_CONFIG);
-	println!("Logging file: {:?}",log_path);
-	match metadata(log_path.as_path()) {
-		Ok(v) => { if v.is_file() { init_file(&log_path); return; } },
-		Err(e) => println!("Error for log config: {:?}",e),
-	}
-	//init_config();
+    println!("Logging file: {:?}",log_path);
+    match metadata(log_path.as_path()) {
+        Ok(v) => { if v.is_file() { init_file(&log_path); return; } },
+        Err(e) => println!("Error for log config: {:?}",e),
+    }
+    init_config(); // call fallback
 }
 
 /// Initialize log config from file
@@ -31,50 +40,56 @@ fn init_file(conf: &Path) {
 }
 
 /// Initialize a fallback configurated logger.
-/// Conisiting of log to conole & if possible to file.
+/// Consisting of log to conole & if possible to file.
 #[cfg(not(test))]
 fn init_config() {
-    /*let console = Box::new(log4rs::appender::ConsoleAppender::builder()
-        .pattern(log4rs::pattern::PatternLayout::new(LOG_PATTERN).unwrap())
-        .build());
+    let stdout_appender = ConsoleAppender::builder()
+    .encoder(Box::new(PatternEncoder::new(LOG_PATTERN))).build();
     
-    let file_appender = log4rs::appender::FileAppender::builder("log/hc_log.log")
-        .pattern(log4rs::pattern::PatternLayout::new(LOG_PATTERN).unwrap()).build();
+    let file_appender = FileAppender::builder()
+    .encoder(Box::new(PatternEncoder::new(LOG_PATTERN))).build("log/default.log");
     let file_success = file_appender.is_ok();
     
-    let mut root = log4rs::config::Root::builder(log::LogLevelFilter::max())
-        .appender("console".to_string());
+    let mut root_builder = Root::builder().appender(APPENDER_STDOUT);
+    
     if file_success {
-        root = root.appender("file".to_string());
-    }else{
+        root_builder = root_builder.appender(APPENDER_FILE);
+    }
+    
+    let root = root_builder.build(LogLevelFilter::max());
+
+    let mut config_builder = Config::builder()
+    .appender(Appender::builder().build(APPENDER_STDOUT, Box::new(stdout_appender)));
+    if file_success {
+        config_builder = config_builder.appender(Appender::builder().build(APPENDER_FILE, Box::new(file_appender.unwrap())));
+    }
+    
+    config_builder = config_builder.logger(Logger::builder().build("hyper", LogLevelFilter::Warn));
+    
+    let config = config_builder.build(root).unwrap();
+    
+    println!("Log fallback init: {}",log4rs::init_config(config).is_ok());
+    
+    if !file_success { // print after log init, useless otherwise
         error!("Could not initialize file based logging!");
     }
     
-    let mut config = log4rs::config::Config::builder(root.build())
-        .appender(log4rs::config::Appender::builder("console".to_string(), console).build());
-    if file_success {
-        let file = Box::new(file_appender.unwrap());
-        config = config.appender(log4rs::config::Appender::builder("file".to_string(), file).build());
-    }
-        
-    println!("{:?}",log4rs::init_config(config.build().unwrap()));
     warn!("No log config file found, please create file {}",LOG_CONFIG);
     warn!("According to https://github.com/sfackler/log4rs");
-    info!("Using internal logging configuration on most verbose level.");*/
+    info!("Using internal logging configuration on most verbose level.");
 }
 
-/// Test logger configuration, without file support, ignoring external config
+/// Test logger configuration, without file support, ignoring external configs
 #[cfg(test)]
 pub fn init_config() {
-    let console = Box::new(log4rs::appender::ConsoleAppender::builder()
-        .pattern(log4rs::pattern::PatternLayout::new(LOG_PATTERN).unwrap())
-        .build());
+    let console = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(LOG_PATTERN))).build();
     
-    let mut root = log4rs::config::Root::builder(log::LogLevelFilter::max())
-        .appender("console".to_string());
+    let mut root = Root::builder().appender(APPENDER_STDOUT)
+        .build(LogLevelFilter::max());
     
-    let mut config = log4rs::config::Config::builder(root.build())
-        .appender(log4rs::config::Appender::builder("console".to_string(), console).build());
+    let mut config = Config::builder()
+        .appender(Appender::builder().build(APPENDER_STDOUT, Box::new(console))).build(root).unwrap();
         
     info!("Test logger configuration");
 }
