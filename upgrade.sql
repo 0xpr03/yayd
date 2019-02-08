789067871495edb8788abf849b19eab6ee4c20aa
Aron Heinecke <aron.heinecke@t-online.de>
2019-02-08 14:21:10 +0100

    Add upgrade plan for utf8mb4
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

---
+++ upgrade.sql
@@ -0,0 +1,35 @@
+/* from 0.6.1 */
+ALTER TABLE
+    `queries`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+    
+ALTER TABLE
+    `playlists`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+    `querydetails`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+    
+ALTER TABLE
+    `files`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+    
+ALTER TABLE
+    `queryerror`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+    
+ALTER TABLE
+    `subqueries`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+    
+ALTER TABLE
+    `query_files`
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
